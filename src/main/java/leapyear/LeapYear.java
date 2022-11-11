9715046e419a1711949fe40209b4d20d540564b9
adrixo <adrianvalrom@gmail.com>
2022-11-11 15:14:27 +0100

    feat: leap year

--- src/main/java/leapyear/LeapYear.java
+++ src/main/java/leapyear/LeapYear.java
@@ -1,4 +1,23 @@
 package leapyear;
 
 public class LeapYear {
+    public static boolean is(int i) {
+        if (isDivisibleBy100(i) && isDivisibleBy400(i))
+            return false;
+        if (isDivisibleBy4(i))
+            return true;
+        return false;
+    }
+
+    private static boolean isDivisibleBy400(int i) {
+        return i % 400 != 0;
+    }
+
+    private static boolean isDivisibleBy100(int i) {
+        return i % 100 == 0;
+    }
+
+    private static boolean isDivisibleBy4(int i) {
+        return isDivisibleBy100(i, 4);
+    }
 }
--- src/test/java/leapyear/LeapYearShould.java
+++ src/test/java/leapyear/LeapYearShould.java
@@ -1,5 +1,56 @@
 package leapyear;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class LeapYearShould {
 
+    @ParameterizedTest
+    @CsvSource({
+            "1997",
+            "1998",
+            "1999"
+    })
+    public void
+    a_non_leap_year_shouldnt_be_divisible_by_4(int year) {
+        assertFalse(LeapYear.is(year));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2000",
+            "2004",
+            "1996"
+    })
+    public void
+    a_leap_year_is_divisible_by_4(int year) {
+        assertTrue(LeapYear.is(year));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "400",
+            "800",
+            "1200",
+            "1600",
+            "2000"
+    })
+    public void
+    a_leap_year_is_divisible_by_400(int year) {
+        assertTrue(LeapYear.is(year));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1400",
+            "1800",
+    })
+    public void
+    a_leap_year_is_not_divisible_by_100_but_400(int year) {
+        assertFalse(LeapYear.is(year));
+    }
 }
